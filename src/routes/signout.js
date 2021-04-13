ec42e9a3cd4f5ff1956a606143eb1f08f6f21515
thatsongsucks <andrew.tisdall@gmail.com>
2021-04-13 14:08:07 -0700

    styling for phones

--- src/routes/signout.js
+++ src/routes/signout.js
@@ -3,8 +3,8 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/signout', (req, res) => {
-    req.session = null;
-    console.log('ok');
+    req.session.jwt = null;
+    console.log('signing out');
 
     res.redirect('/signin');
 });
