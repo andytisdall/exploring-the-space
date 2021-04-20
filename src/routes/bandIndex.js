2ef547f2704401296022dff71042bd3431c2b239
thatsongsucks <andrew.tisdall@gmail.com>
2021-04-20 13:53:06 -0700

    bug was not showing all versions

--- src/routes/bandIndex.js
+++ src/routes/bandIndex.js
@@ -29,6 +29,13 @@ router.get('/:bandName', currentUser, async (req, res) => {
         }   
     });
 
+    const playlists = await Band.findOne({ name: bandName }).populate({
+        path: 'playlists', populate: {
+            path: 'songs'
+        }
+    });
+
+
     if (!band) {
         return res.redirect('/signin');
     }
@@ -38,13 +45,13 @@ router.get('/:bandName', currentUser, async (req, res) => {
         const thisUser = await User.findById(req.currentUser.id);
 
         if (thisUser.bands.includes(band.id)) {
-            res.render('index', {band, errorMessage});
+            res.render('index', {band, playlists, errorMessage});
         } else {
-            res.render('guestIndex', { band, errorMessage });
+            res.render('guestIndex', { band, playlists, errorMessage });
         }
 
     } else {
-        res.render('guestIndex', { band, errorMessage });
+        res.render('guestIndex', { band, playlists, errorMessage });
     }
 
 
