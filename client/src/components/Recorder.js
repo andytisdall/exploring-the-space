a26d9ddcec63de6fb94c5e2254e5b40f60243020
thatsongsucks <andrew.tisdall@gmail.com>
2022-02-12 13:10:18 -0800

    new recorder

--- client/src/components/Recorder.js
+++ client/src/components/Recorder.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react';
-import vmsg from 'vmsg';
+// import vmsg from 'vmsg';
+import WebAudioRecorder from '../web-audio-recorder/WebAudioRecorder';
 
 import Playhead from './Playhead';
 import Timer from './Timer';
@@ -7,20 +8,51 @@ import DeviceControl from './DeviceControl';
 import AddRecording from './AddRecording';
 
 const Recorder = ({ match }) => {
-  const [isLoading, setIsLoading] = useState(false);
   const [isRecording, setIsRecording] = useState(false);
   const [audio, setAudio] = useState();
   const [inputSource, setInputSource] = useState();
   const [error, setError] = useState('');
 
   const mediaRecorder = useRef();
+  const audioContext = useRef(new AudioContext());
 
   useEffect(async () => {
     // Initialize vmsg recorder
-    mediaRecorder.current = new vmsg.Recorder({
-      wasmURL: 'https://unpkg.com/vmsg@0.3.0/vmsg.wasm',
-    });
-  }, []);
+    // mediaRecorder.current = new vmsg.Recorder({
+    //   wasmURL: 'https://unpkg.com/vmsg@0.3.0/vmsg.wasm',
+    // });
+    if (inputSource) {
+      const inputStream = await navigator.mediaDevices.getUserMedia({
+        audio: {
+          deviceId: inputSource.deviceId,
+        },
+      });
+      const node = audioContext.current.createMediaStreamSource(inputStream);
+      mediaRecorder.current = new WebAudioRecorder(node, {
+        workerDir: process.env.PUBLIC_URL + '/web-audio-recorder/',
+        encoding: 'mp3',
+        options: {
+          timeLimit: 18000,
+        },
+        encodeAfterRecord: true,
+        onComplete: (recorder, blob) => {
+          console.log(blob);
+          setAudio(blob);
+          setIsRecording(false);
+        },
+        onTimeout: (recorder) => {
+          const time = recorder.recordingTime();
+          setError(`timeout at ${time} seconds`);
+        },
+        onError: (recorder, message) => {
+          setError(message);
+        },
+        onEncoderLoaded: () => {
+          console.log('encoder loaded');
+        },
+      });
+    }
+  }, [inputSource]);
 
   const onRecord = async () => {
     // setError(isLoading);
@@ -28,26 +60,23 @@ const Recorder = ({ match }) => {
     // setIsLoading(true);
     if (!isRecording) {
       try {
-        await navigator.mediaDevices.getUserMedia({
-          audio: {
-            deviceId: inputSource.deviceId,
-          },
-        });
-        await mediaRecorder.current.initAudio();
-        await mediaRecorder.current.initWorker();
         mediaRecorder.current.startRecording();
+        // await mediaRecorder.current.initAudio();
+        // await mediaRecorder.current.initWorker();
+        // mediaRecorder.current.startRecording();
         // setIsLoading(false);
         setIsRecording(true);
       } catch (e) {
         console.error(e);
         // setIsLoading(false);
-        setError(e);
+        setError(e.message);
       }
     } else {
-      const blob = await mediaRecorder.current.stopRecording();
-      setAudio(blob);
+      mediaRecorder.current.finishRecording();
+      // const blob = await mediaRecorder.current.stopRecording();
+      // setAudio(blob);
       // setIsLoading(false);
-      setIsRecording(false);
+      // setIsRecording(false);
     }
   };
 
