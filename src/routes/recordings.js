3818bf34c81a511dde0273e06257d055cd1e0411
thatsongsucks <andrew.tisdall@gmail.com>
2022-02-25 15:00:05 -0800

    try to log whats happening in the get recordings route

--- src/routes/recordings.js
+++ src/routes/recordings.js
@@ -18,6 +18,7 @@ router.get('/recordings/:id', async (req, res) => {
   const stream = bucket.openDownloadStream(mp3Id);
 
   if (!stream) {
+    console.log('!stream');
     throw new Error('stream not found');
   }
 
