1e701e9fbdbb3a691da6c83d4ab6821beb8375a4
thatsongsucks <andrew.tisdall@gmail.com>
2022-06-06 17:21:35 -0700

    encapsulated mp3 processing logic in the bounce action creators

--- client/src/actions/bounces.js
+++ client/src/actions/bounces.js
@@ -12,6 +12,36 @@ import { errorHandler } from './errors';
 import { selectBounce } from './titles';
 import greenhouse from '../apis/greenhouse';
 
+const processMp3 = (formValues, next) => {
+  const file = formValues.file[0];
+
+  const reader = new FileReader();
+
+  reader.onload = (event) => {
+    const audioContext = new (window.AudioContext ||
+      window.webkitAudioContext)();
+
+    audioContext.decodeAudioData(event.target.result, async (buffer) => {
+      // Obtain the duration in seconds of the audio file
+      const duration = parseInt(buffer.duration);
+      const formObject = { ...formValues, duration, file };
+
+      const formData = new FormData();
+      for (let key in formObject) {
+        formData.append(key, formObject[key]);
+      }
+      // continue dispatching the action
+      next(formData);
+    });
+  };
+  // In case the file couldn't be read
+  reader.onerror = (event) => {
+    console.error('An error ocurred reading the file: ', event);
+  };
+
+  reader.readAsArrayBuffer(file);
+};
+
 export const fetchBounces = (versionId) => async (dispatch) => {
   try {
     const response = await greenhouse.get(`/bounces/${versionId}`);
@@ -21,169 +51,80 @@ export const fetchBounces = (versionId) => async (dispatch) => {
   }
 };
 
-export const createBounce =
-  (formValues, versionId) => async (dispatch, getState) => {
-    dispatch({ type: UPLOAD_STARTED });
-    try {
-      const { currentBand } = getState().bands;
-      const parentVersion = getState().versions[versionId];
+export const createBounce = (formValues, versionId) => (dispatch, getState) => {
+  dispatch({ type: UPLOAD_STARTED });
+  const { currentBand } = getState().bands;
+  const parentVersion = getState().versions[versionId];
 
-      if (!parentVersion.bounces.length) {
-        formValues.latest = true;
+  if (!parentVersion.bounces.length) {
+    formValues.latest = true;
+  }
+  formValues.currentBand = currentBand.id;
+  formValues.version = versionId;
+
+  processMp3(formValues, dispatchAction);
+
+  // call this to finish dispatching after the mp3 is processed
+  async function dispatchAction(formData) {
+    try {
+      const response = await greenhouse.post('/bounces', formData, {
+        headers: { 'Content-Type': 'multipart/form-data' },
+      });
+
+      if (response.data.latest) {
+        if (parentVersion.bounces.length) {
+          const bounceList = parentVersion.bounces.map(
+            (id) => getState().bounces[id]
+          );
+          const oldLatest = bounceList.find((b) => b.latest);
+          oldLatest.latest = false;
+          dispatch(
+            editBounce(
+              _.pick(oldLatest, 'date', 'comments', 'latest'),
+              oldLatest.id,
+              versionId
+            )
+          );
+        }
       }
 
-      // if (formValues.file instanceof FileList) {
-      //   formValues.file = formValues.file[0];
-      // }
-
-      formValues.file = formValues.file[0];
-
-      // Create instance of FileReader
-      const reader = new FileReader();
-
-      // When the file has been succesfully read
-      reader.onload = (event) => {
-        // Create an instance of AudioContext
-        const audioContext = new (window.AudioContext ||
-          window.webkitAudioContext)();
-
-        // Asynchronously decode audio file data contained in an ArrayBuffer.
-        audioContext.decodeAudioData(event.target.result, async (buffer) => {
-          // Obtain the duration in seconds of the audio file (with milliseconds as well, a float value)
-          const duration = parseInt(buffer.duration);
-
-          const formObject = {
-            ...formValues,
-            currentBand: currentBand.id,
-            version: versionId,
-            duration,
-          };
-
-          const formData = new FormData();
-
-          for (let key in formObject) {
-            formData.append(key, formObject[key]);
-          }
-
-          const response = await greenhouse.post('/bounces', formData, {
-            headers: { 'Content-Type': 'multipart/form-data' },
-          });
-
-          if (response.data.latest) {
-            if (parentVersion.bounces.length) {
-              const bounceList = parentVersion.bounces.map(
-                (id) => getState().bounces[id]
-              );
-              const oldLatest = bounceList.find((b) => b.latest);
-              oldLatest.latest = false;
-              dispatch(
-                editBounce(
-                  _.pick(oldLatest, 'date', 'comments', 'latest'),
-                  oldLatest.id,
-                  versionId
-                )
-              );
-            }
-          }
-
-          // dispatch({ type: SELECT_BOUNCE, payload: { titleId, bounce: response.data } });
-          dispatch({
-            type: CREATE_BOUNCE,
-            payload: { bounce: response.data, version: versionId },
-          });
-        });
-      };
-
-      // In case the file couldn't be read
-      reader.onerror = (event) => {
-        console.error('An error ocurred reading the file: ', event);
-      };
-
-      // Read file as an ArrayBuffer, important !
-      reader.readAsArrayBuffer(formValues.file);
+      dispatch({
+        type: CREATE_BOUNCE,
+        payload: { bounce: response.data, version: versionId },
+      });
     } catch (err) {
       dispatch({ type: UPLOAD_FAILURE });
       dispatch(errorHandler(err));
     }
-  };
+  }
+};
 
 export const editBounce =
-  (formValues, bounceId, versionId) => async (dispatch, getState) => {
-    try {
-      const { currentBand } = getState().bands;
-      const thisBounce = getState().bounces[bounceId];
-      if (thisBounce.latest) {
-        formValues.latest = true;
-      }
-      if (formValues.file && formValues.file.length) {
-        formValues.file = formValues.file[0];
+  (formValues, bounceId, versionId) => (dispatch, getState) => {
+    const { currentBand } = getState().bands;
+    const thisBounce = getState().bounces[bounceId];
 
-        // Create instance of FileReader
-        const reader = new FileReader();
+    if (thisBounce.latest) {
+      formValues.latest = true;
+    }
+    formValues.currentBand = currentBand.id;
 
-        // When the file has been succesfully read
-        reader.onload = (event) => {
-          // Create an instance of AudioContext
-          const audioContext = new (window.AudioContext ||
-            window.webkitAudioContext)();
+    let requestOptions = {};
+    if (formValues.file?.length) {
+      requestOptions.headers = { 'Content-Type': 'multipart/form-data' };
+      processMp3(formValues, dispatchAction);
+    } else {
+      dispatchAction(formValues);
+    }
 
-          // Asynchronously decode audio file data contained in an ArrayBuffer.
-          audioContext.decodeAudioData(event.target.result, async (buffer) => {
-            // Obtain the duration in seconds of the audio file (with milliseconds as well, a float value)
-            const duration = parseInt(buffer.duration);
+    async function dispatchAction(formData) {
+      try {
+        const response = await greenhouse.patch(
+          `/bounces/${bounceId}`,
+          formData,
+          requestOptions
+        );
 
-            const formObject = {
-              ...formValues,
-              currentBand: currentBand.id,
-              duration,
-            };
-
-            const formData = new FormData();
-
-            for (let key in formObject) {
-              formData.append(key, formObject[key]);
-            }
-
-            const response = await greenhouse.patch(
-              `/bounces/${bounceId}`,
-              formData,
-              {
-                headers: { 'Content-Type': 'multipart/form-data' },
-              }
-            );
-
-            if (response.data.latest && !thisBounce.latest) {
-              const parentVersion = getState().versions[versionId];
-              const bounceList = parentVersion.bounces.map(
-                (id) => getState().bounces[id]
-              );
-              const oldLatest = bounceList.find((b) => b.latest);
-              oldLatest.latest = false;
-              dispatch(
-                editBounce(
-                  _.pick(oldLatest, 'date', 'comments', 'latest'),
-                  oldLatest.id,
-                  versionId
-                )
-              );
-            }
-
-            dispatch({ type: EDIT_BOUNCE, payload: response.data });
-          });
-        };
-
-        // In case the file couldn't be read
-        reader.onerror = (event) => {
-          console.error('An error ocurred reading the file: ', event);
-        };
-
-        // Read file as an ArrayBuffer, important !
-        reader.readAsArrayBuffer(formValues.file);
-      } else {
-        const response = await greenhouse.patch(`/bounces/${bounceId}`, {
-          ...formValues,
-          currentBand: currentBand.id,
-        });
         if (response.data.latest && !thisBounce.latest) {
           const parentVersion = getState().versions[versionId];
           const bounceList = parentVersion.bounces.map(
@@ -201,9 +142,9 @@ export const editBounce =
         }
 
         dispatch({ type: EDIT_BOUNCE, payload: response.data });
+      } catch (err) {
+        dispatch(errorHandler(err));
       }
-    } catch (err) {
-      dispatch(errorHandler(err));
     }
   };
 
--- src/routes/bounces.js
+++ src/routes/bounces.js
@@ -7,7 +7,6 @@ import { bucket } from './audio.js';
 import { requireAuth } from '../middlewares/require-auth.js';
 import { currentUser } from '../middlewares/current-user.js';
 
-
 const Song = mongoose.model('Song');
 const Version = mongoose.model('Version');
 const PlaylistSong = mongoose.model('PlaylistSong');
@@ -15,29 +14,94 @@ const PlaylistSong = mongoose.model('PlaylistSong');
 const router = express.Router();
 
 router.post('/bounces', currentUser, requireAuth, async (req, res) => {
+  // Increase timeout length for long uploads
 
-    // Increase timeout length for long uploads
+  req.socket.setTimeout(10 * 60 * 1000);
 
-    req.socket.setTimeout(10 * 60 * 1000);
+  // Get parameters from post request and exit if there's an existing bounce with that date.
 
-    // Get parameters from post request and exit if there's an existing bounce with that date.
-
-    const { date, comments, duration, version, latest } = req.body;
-    const parentVersion = await Version.findById(version).populate('songs');
-    if (parentVersion.songs)  {
-        let duplicateDate = parentVersion.songs.find(b => b.date === date);
-        if (duplicateDate) {
-            throw new Error('Duplicate date found in the version list');
-        }
+  const { date, comments, duration, version, latest } = req.body;
+  const parentVersion = await Version.findById(version).populate('songs');
+  if (parentVersion.songs) {
+    let duplicateDate = parentVersion.songs.find((b) => b.date === date);
+    if (duplicateDate) {
+      throw new Error('Duplicate date found in the version list');
     }
+  }
 
+  const file = req.files.file;
+
+  // Create a new bounce object form values
+
+  const newBounce = new Song({
+    date,
+    comments,
+    size: file.size,
+    duration,
+    latest,
+  });
+
+  // Create upload stream object
+  let stream = bucket.openUploadStream(file.name);
+
+  const readableStream = new Readable();
+  readableStream.push(file.data);
+  readableStream.push(null);
+  readableStream.pipe(stream);
+
+  stream.on('error', (err) => {
+    throw new Error('Error uploading mp3!');
+  });
+
+  // Finish up on completed upload
+  stream.on('finish', async () => {
+    // Get id of mp3 from stream object
+    newBounce.mp3 = stream.id;
+
+    // Add bounce to parent version's bounce list
+    parentVersion.songs.push(newBounce);
+
+    // Finally save new bounce object
+    await newBounce.save();
+    await parentVersion.save();
+
+    console.log('Uploaded & created bounce record:', newBounce);
+
+    return res.status(201).send(newBounce);
+  });
+});
+
+router.get('/bounces/:versionId', async (req, res) => {
+  const version = await Version.findById(req.params.versionId).populate(
+    'songs'
+  );
+
+  res.status(200).send(version.songs);
+});
+
+router.patch('/bounces/:id', currentUser, requireAuth, async (req, res) => {
+  const { id } = req.params;
+  const { date, comments, duration, latest } = req.body;
+
+  const thisBounce = await Song.findById(id);
+
+  thisBounce.comments = comments;
+  thisBounce.date = date;
+  thisBounce.latest = latest;
+
+  //
+
+  if (req.files) {
     const file = req.files.file;
 
-    // Create a new bounce object form values
+    // delete old mp3
 
-    const newBounce = new Song({date, comments, size: file.size, duration, latest});
+    const mp3Id = new mongodb.ObjectID(thisBounce.mp3);
 
+    // Edit bounce object form values
 
+    thisBounce.size = file.size;
+    thisBounce.duration = duration;
 
     // Create upload stream object
     let stream = bucket.openUploadStream(file.name);
@@ -47,140 +111,59 @@ router.post('/bounces', currentUser, requireAuth, async (req, res) => {
     readableStream.push(null);
     readableStream.pipe(stream);
 
-
     stream.on('error', (err) => {
-        throw new Error('Error uploading mp3!');
+      throw new Error('Error uploading mp3!');
     });
 
     // Finish up on completed upload
     stream.on('finish', async () => {
+      bucket.delete(mp3Id, async (err) => {
+        if (err) {
+          throw new Error('Error attempting to delete mp3');
+        } else {
+          console.log('mp3 deleted');
 
-        // Get id of mp3 from stream object
-        newBounce.mp3 = stream.id;
+          // Get id of mp3 from stream object
+          thisBounce.mp3 = stream.id;
+          await thisBounce.save();
 
-
-        // Add bounce to parent version's bounce list
-        parentVersion.songs.push(newBounce);
-
-        // Finally save new bounce object
-        await newBounce.save();
-        await parentVersion.save();
-
-        console.log('Uploaded & created bounce record:', newBounce);
-
-        return res.status(201).send(newBounce);
+          res.send(thisBounce);
+        }
+      });
     });
-
-});
-
-
-router.get('/bounces/:versionId', async (req, res) => {
-
-    const version = await Version.findById(req.params.versionId).populate('songs');
-    
-    res.status(200).send(version.songs);
-
-});
-
-
-router.patch('/bounces/:id', currentUser, requireAuth, async (req, res) => {
-
-    const { id } = req.params;
-    const { date, comments, duration, latest } = req.body;
-
-    const thisBounce = await Song.findById(id);
-
-    thisBounce.comments = comments;
-    thisBounce.date = date;
-    thisBounce.latest = latest;
-
-    //
-
-
-    if (req.files) {
-        const file = req.files.file;
-
-        // delete old mp3
-
-        const mp3Id = new mongodb.ObjectID(thisBounce.mp3);
-
-
-
-        // Edit bounce object form values
-
-        thisBounce.size = file.size;
-        thisBounce.duration = duration;
-
-        // Create upload stream object
-        let stream = bucket.openUploadStream(file.name);
-
-        const readableStream = new Readable();
-        readableStream.push(file.data);
-        readableStream.push(null);
-        readableStream.pipe(stream);
-
-
-        stream.on('error', (err) => {
-            throw new Error('Error uploading mp3!')
-        });
-
-        // Finish up on completed upload
-        stream.on('finish', async () => {
-
-            bucket.delete(mp3Id, async (err) => {
-                if (err) {
-                    throw new Error('Error attempting to delete mp3');
-                } else {
-                    console.log('mp3 deleted');
-
-                    // Get id of mp3 from stream object
-                    thisBounce.mp3 = stream.id;
-                    await thisBounce.save();
-
-                    res.send(thisBounce);
-                }
-            });
-
-
-            
-        });
-
-    } else {
-        await thisBounce.save();
-        res.send(thisBounce);
-    }
+  } else {
+    await thisBounce.save();
+    res.send(thisBounce);
+  }
 });
 
 router.post('/bounces/delete', currentUser, requireAuth, async (req, res) => {
-    const { bounceId, versionId } = req.body;
+  const { bounceId, versionId } = req.body;
 
-    const thisBounce = await Song.findById(bounceId);
-    const mp3Id = new mongodb.ObjectID(thisBounce.mp3);
-    const parentVersion = await Version.findById(versionId);
-    if (parentVersion) {
-        await Version.updateOne({ _id: versionId }, { $pull: { songs: bounceId } });
+  const thisBounce = await Song.findById(bounceId);
+  const mp3Id = new mongodb.ObjectID(thisBounce.mp3);
+  const parentVersion = await Version.findById(versionId);
+  if (parentVersion) {
+    await Version.updateOne({ _id: versionId }, { $pull: { songs: bounceId } });
+  }
+  const playlistSongs = await PlaylistSong.find({ bounce: bounceId });
+
+  playlistSongs.forEach(async (pls) => {
+    pls.bounce = null;
+    await pls.save();
+  });
+
+  bucket.delete(mp3Id, (err) => {
+    if (err) {
+      throw new Error('Error attempting to delete mp3');
+    } else {
+      console.log('mp3 deleted');
     }
-    const playlistSongs = await PlaylistSong.find({ bounce: bounceId });
+  });
 
-    playlistSongs.forEach(async pls => {
-        pls.bounce = null;
-        await pls.save();
-    });
+  await Song.deleteOne({ _id: bounceId });
 
-    
-    bucket.delete(mp3Id, (err) => {
-        if (err) {
-            throw new Error('Error attempting to delete mp3');
-        } else {
-            console.log('mp3 deleted');
-        }
-    });
-
-
-    await Song.deleteOne({ _id: bounceId });
-
-    res.send(thisBounce);
-
+  res.send(thisBounce);
 });
 
 export { router as bounceRouter };
