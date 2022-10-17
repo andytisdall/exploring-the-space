3dee52bb2310d50ae4697d4107e7261b041ee086
thatsongsucks <andrew.tisdall@gmail.com>
2022-10-17 12:58:30 -0700

    fix time formatting in playcontainer

--- client/src/components/titles/PlayContainer.js
+++ client/src/components/titles/PlayContainer.js
@@ -14,11 +14,11 @@ const PlayContainer = ({ song, queueSongs, parentType }) => {
 
   const displayTime = (time) => {
     const minutes = Math.floor(time / 60);
-    if (minutes) {
-      const seconds =
-        Math.floor(time % 60) < 10
-          ? '0' + Math.floor(time % 60)
-          : Math.floor(time % 60);
+    const seconds =
+      Math.floor(time % 60) < 10
+        ? '0' + Math.floor(time % 60)
+        : Math.floor(time % 60);
+    if (minutes || seconds) {
       return `${minutes}:${seconds}`;
     }
   };
